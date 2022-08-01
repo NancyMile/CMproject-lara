b9f9893c0b92d076be434ea0002bec49f8bef5d7
nancy <smithnancy@outlook.com>
2022-08-01 17:05:44 +1000

    Seeders for companies and employees created

---
+++ database/seeds/CompanyTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+class CompanyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        //$storage = Storage::fake('app/public/images');
+        //
+        for ($i = 1; $i <= 3; $i++) {
+            DB::table('companies')->insert([
+                'name' => $faker->company,
+                'email' => $faker->email,
+                //'logo' => $faker->image(storage_path($storage), 100, 100, null, false),
+                
+                'logo' =>$faker->image('storage/app/public/images', 400, 300, null, false),
+                
+                
+                'website' => $faker->url,
+                'created_at'=>$faker->dateTimeBetween($startDate = '-24 month',$endDate = 'now'),
+                'updated_at'=>$faker->dateTimeBetween($startDate = '-3 day',$endDate = 'now')
+                ]);
+        }    
+    }
+}
---
+++ database/seeds/EmployeesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Company;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+
+class EmployeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i = 1; $i <=3; $i++) {
+            DB::table('employees')->insert([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'company_id' => Company::all()->random()->id,
+                'email' => $faker->email,
+                'phone' => $faker->numerify('#########'),
+                'created_at'=>$faker->dateTimeBetween($startDate = '-24 month',$endDate = 'now'),
+                'updated_at'=>$faker->dateTimeBetween($startDate = '-3 day',$endDate = 'now')
+            ]);
+        }  
+    }
+}

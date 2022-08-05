d75a6eaa626ee6df93afd8f6848839000edc4d3c
nancy <smithnancy@outlook.com>
2022-08-05 23:54:30 +1000

    admin layout  icon added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,8 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CM-Project</title>
+  <link rel="icon" type="image/x-icon" href="{{asset('dist/img/icons.png')}}"/>
 
-  <!-- Google Font: Source Sans Pro NANCYYYYYYYY  {{ asset('css/app.css') }} -->
+  <!-- Google Font: Source Sans Pro
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">

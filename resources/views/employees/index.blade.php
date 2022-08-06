04f962b65a3d33bf153609d7b758327527352310
nancy <smithnancy@outlook.com>
2022-08-06 16:50:25 +1000

    employees  list pagination added

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
-
+@section('title','Employees List')
 @section('content')
 <!-- Content Header (Page header) -->
 <div class="content-header">
@@ -7,6 +7,9 @@
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0">Dashboard</h1>
+            @if (Session::has('success'))
+                <div class="alert alert-success">{{ Session::get('success') }}</div>
+            @endif
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
@@ -23,13 +26,16 @@
         </h3>
         <div class="card-tools">
             <ul class="pagination pagination-sm">
-            <!-- {{$employees->links()}} -->
+            {{ $employees->links() }}
             </ul>
         </div>
         </div>
         <!-- /.card-header -->
         <div class="card-body">
         <ul class="todo-list" data-widget="todo-list">
+
+        {{-- dd($employees) --}}
+
         @forelse($employees as $employee)
             <li>
             <span class="handle">
@@ -37,7 +43,7 @@
                 <i class="fas fa-ellipsis-v"></i>
             </span>
             <span class="text"><a href="{{ route('employees.show',$employee)}}"> {{$employee->first_name}} {{$employee->last_name}}</span>
-            <small class="badge"><i class="far fa-clock"></i> {{$employee->email}}</small>
+            <small class="badge"><i class="far fa-clock"></i> {{$employee->company->name}}</small>
             <div class="tools">
                 <a href="{{route('employees.edit', $employee)}}"><i class="fas fa-edit"></i></a>
                 <form action="{{route('employees.destroy',$employee)}}" method="POST">
